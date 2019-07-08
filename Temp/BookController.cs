a3b2049375018865c86f2ae4814c5e459040af21
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2019-07-08 13:03:43 +0800

    Temp

---
+++ Temp/BookController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManagement.Controllers
+{
+	public class BookController : Controller
+	{
+		readonly Models.CodeService codeService = new Models.CodeService();
+		readonly Models.BookService bookService = new Models.BookService();
+
+		int bookid_byinsert;
+		// GET: BookByAJAX
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public JsonResult GetBookClassDropDownListData()
+		{
+			return Json(this.codeService.GetBookClassName());
+		}
+		[HttpPost]
+		public JsonResult GetLendUserDropDownListData()
+		{
+			return Json(this.codeService.GetUserName());
+		}
+		[HttpPost]
+		public JsonResult GetBookStatusDropDownListData()
+		{
+			return Json(this.codeService.GetCodeName());
+		}
+		[HttpPost]
+		public JsonResult GetSearchGrid(Models.BookSearchArg bookscondition)
+		{
+			return Json(bookService.GetBookByCondtioin(bookscondition));
+		}
+		[HttpPost]
+		public JsonResult DeleteBook(int bookid)
+		{
+			return Json(bookService.DeleteBook(bookid));
+		}
+
+		[HttpPost]
+		public JsonResult GetBookRecordByBookID(int bookid)
+		{
+			return Json(bookService.GetBookLendRecord(bookid));
+		}
+
+		public ActionResult InsertBook()
+		{
+			return View();
+		}
+
+		public JsonResult AddBook(Models.Books book)
+		{
+			//bookid_byinsert = bookService.InsertBook(book);
+			return Json(bookService.InsertBook(book));
+		}
+
+		//public JsonResult ShowBookData(int book)
+		[HttpPost]
+		public ActionResult EditBook(int bookid)
+		{
+			ViewData["bookid"] = bookid;
+			return View();
+		}
+
+		[HttpPost]
+		public JsonResult GetOriginBookData(int bookid)
+		{
+			return Json(this.bookService.GetBookData(bookid));
+		}
+	}
+}
---
+++ Temp/BookRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
+{
+	public class BookRecord
+	{
+		[DisplayName("借閱日期")]
+		public string LendDate { get; set; }
+
+		[DisplayName("借閱人員編號")]
+		public string LendUserId { get; set; }
+
+		[DisplayName("英文姓名")]
+		public string LendUserCName { get; set; }
+
+		[DisplayName("中文姓名")]
+		public string LendUserEName { get; set; }
+	}
+}
